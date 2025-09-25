86d708ce33f6a085602e59ec7055a3291c624a48
Daniel Gomes <dannyelgjl@gmail.com>
2025-09-25 05:22:41 -0300

    fix: adjustment routes

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Input } from './Input';
+export { default as Select } from './Select';
+export { default as ExpenseList } from './ExpenseList';
+export { default as Header } from './Header';
+export * from './Icon';
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from '../screens/Home';
+import EnterExpenses from '../screens/EnterExpenses';
+import DetailExpense from '../screens/DetailExpense';
+import { ParamListBase } from '@react-navigation/native';
+import { ExpenseItem } from '../services/data';
 
-export interface PropsRootStack {
+export interface PropsRootStack extends ParamListBase {
   Home: undefined;
+  EnterExpenses: undefined;
+  DetailExpense: { expense: ExpenseItem };
 }
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const Routes = () => {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
       <Screen name="Home" component={Home} />
+
+      <Screen name="EnterExpenses" component={EnterExpenses} />
+      <Screen name="DetailExpense" component={DetailExpense} />
     </Navigator>
   );
 };
